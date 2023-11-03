b4d6422e8cfaa9286d60c5d2011f734e3d1a66fe
AstroPilot2808 <ajayohm28@gmail.com>
2023-11-03 14:16:53 -0400

    maze.py_implementationof_maze-solving

---
+++ maze.py
@@ -0,0 +1,9 @@
+#Implementation of Maze Solving Algorithms (Tree Traversals)
+
+import sys
+
+class Node():
+    def __init__(self, state, parent, action):
+        self.state = state
+        self.parent = parent
+        self.action = action
